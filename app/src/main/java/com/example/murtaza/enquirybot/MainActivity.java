e532983331a4ea2137d20866a16c7e82986ba9f3
murtaza98 <m.patrawala@somaiya.edu>
2019-01-12 01:09:20 +0530

    Initialize

---
+++ app/src/main/java/com/example/murtaza/enquirybot/MainActivity.java
@@ -0,0 +1,49 @@
+package com.example.murtaza.enquirybot;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        startActivity(new Intent(MainActivity.this, ChatWindow.class));
+
+        Button btn_refresh = (Button)findViewById(R.id.btn_refresh);
+        btn_refresh.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String wifi_ssid = getCurrentSsid(getApplicationContext());
+                if(wifi_ssid!=null){
+                    Toast.makeText(MainActivity.this, "WIFI connected NAME "+wifi_ssid, Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    public static String getCurrentSsid(Context context) {
+        String ssid = null;
+        ConnectivityManager connManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
+        if (networkInfo.isConnected()) {
+            final WifiManager wifiManager = (WifiManager) context.getSystemService(Context.WIFI_SERVICE);
+            final WifiInfo connectionInfo = wifiManager.getConnectionInfo();
+            if (connectionInfo != null && !TextUtils.isEmpty(connectionInfo.getSSID())) {
+                ssid = connectionInfo.getSSID();
+            }
+        }
+        return ssid;
+    }
+}
